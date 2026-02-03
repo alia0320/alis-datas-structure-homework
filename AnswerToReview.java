d3ac38afe625a6ffe11c2b02349ffb8e2219d0cd
Ali Ahmed <alirahmed0320@gmail.com>
2026-02-03 13:54:34 -0500

    first commit

---
+++ AnswerToReview.java
@@ -0,0 +1,14 @@
+package hw1;
+
+import java.util.Set;
+
+public class AnswerToReview {
+
+	public static void main(String[] args) {
+		System.out.println("The function has an O(n) time complexity."); // answer to question 1
+		System.out.println("The function has an O(n^2) time complexity."); // answer to question 2
+		System.out.println("The function has an O(a + b) time complexity."); // answer to question 3
+		System.out.println("The function has an O(n^2) time complexity."); // answer to question 4
+		System.out.println("The function has an O(n^2) time complexity."); // answer to question 5
+	}
+}
